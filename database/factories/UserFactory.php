a932485b36df54fb6277539f0d0e2f935aa92a4e
Andrew Feeney <andrew@deskspacecreative.com.au>
2022-11-15 20:41:03 +1100

    Add handle to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,6 +15,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'handle' => Str::slug($this->faker->name() . random_int(0, 99)),
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
